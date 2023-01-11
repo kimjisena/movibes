5875a8e4ac52ab9af336be4c45603d791398d063
Kim Jisena <pjohmaths@gmail.com>
2023-01-11 15:55:52 +0300

    api: test tmdb api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,11 @@ type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await fetch('https://api.themoviedb.org/3/movie/550?api_key=de925752aa6bd33c06cb480db614f6ba');
+  const data = await response.json();
+  return res.status(200).json(data);
 }
